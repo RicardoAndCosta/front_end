1de48573014776e9f761b137dfdf05f0cd979047
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2024-09-07 10:55:06 -0300

    Inicio dos estudos em Angular

---
+++ angular/frontend/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MatToolbarModule
+  ],
+  templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+}
